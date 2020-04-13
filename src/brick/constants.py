74148a9228cb0f020a7083ffbbff9bdb03a5a587
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2020-04-13 10:49:50 +0200

    fix constants for automatic tests

--- src/brick/constants.py
+++ src/brick/constants.py
@@ -1,12 +1,12 @@
 DEFAULT_CONFIG = """{
-    "name": "brick"
+    "name": "brick",
     // "mode": "config"
     "network": {
         // "interface": "wifi"
         "hotspot": {
             // "ssid": "esp32"
             // "password": "changeme"
-        }
+        },
         "wifi": {
             // "ssid": "mynetwork"
             // "password": "mypassword"
