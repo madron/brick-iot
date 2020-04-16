f390a52dac52bacfdda584b8fb089bae119f0c86
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2020-04-17 00:19:01 +0200

    refactor device

--- src/brick/device/__init__.py
+++ src/brick/device/__init__.py
@@ -1,16 +1,49 @@
 import uasyncio as asyncio
-from brick.device import test
+import ure as re
+from brick.exceptions import ValidationError
 
 
-DEVICE_CLASS = {
-    'test-random': test.Random,
-    'test-state-publish': test.StatePublish,
-}
+def import_device_modules():
+    from brick.device import test
 
 
-def get_device_instance(name, config):
-        device_class = DEVICE_CLASS[config.pop('type')]
-        return device_class(**config)
+_device_registry = dict()
+
+
+def register_device(device_type=None):
+    name = device_type
+    def decorator(device_class):
+        device_type = name or device_class.__name__
+        if device_type in _device_registry:
+            raise KeyError('device_type {} is already present.'.format(device_type))
+        _device_registry[device_type] = device_class
+        return device_class
+    return decorator
+
+
+def validate_device(config):
+    re_name = re.compile('^[a-z][a-z0-9_]*$')
+    import_device_modules()
+    devices = dict()
+    errors = dict()
+    for device_name, device_config in config.items():
+        try:
+            if not re_name.match(device_name):
+                raise ValidationError('Name must contains lowercase characters, numbers and _ only.')
+            if 'type' not in device_config:
+                raise ValidationError('type parameter is required.')
+            device_type = device_config.pop('type')
+            if device_type not in _device_registry:
+                raise ValidationError("type '{}' does not exist.".format(device_type))
+            device_class = _device_registry[device_type]
+            devices[device_name] = device_class(**device_config)
+        except ValidationError as error:
+            errors[device_name] = error
+        except Exception as error:
+            errors[device_name] = ValidationError(str(error))
+    if errors:
+        raise ValidationError(errors)
+    return devices
 
 
 class DeviceManager:
@@ -20,15 +53,10 @@ class DeviceManager:
         self.log = self.log_collector.get_logger('devicemanager')
         self.broker = self.dispatcher.get_broker('devicemanager')
         self.config = config
-        self.devices = dict()
-        for device_name, device_config in config.items():
-            try:
-                instance = get_device_instance(device_name, device_config)
-                instance.log = self.log_collector.get_logger(device_name)
-                instance.broker = self.dispatcher.get_broker(device_name,callback=instance._message_received)
-                self.devices[device_name] = instance
-            except Exception as error:
-                self.log.exception('Device {} discarded'.format(device_name), error)
+        self.devices = validate_device(config)
+        for device_name, instance in self.devices.items():
+            instance.log = self.log_collector.get_logger(device_name)
+            instance.broker = self.dispatcher.get_broker(device_name,callback=instance._message_received)
 
     async def start(self):
         for name, device in self.devices.items():
@@ -39,3 +67,72 @@ class DeviceManager:
         for device in self.devices.values():
             await device.stop()
             await asyncio.sleep(0)
+
+
+class Device:
+    async def start(self):
+        self.log.debug('Started')
+        await self._setup()
+        self._task = asyncio.create_task(self._loop())
+
+    async def stop(self):
+        await self._teardown()
+        self._task.cancel()
+        self.log.debug('Stopped')
+
+    async def _setup(self):
+        # Check log and broker
+        assert bool(self.log)
+        assert bool(self.broker)
+        # State
+        self._state = dict()
+        self._subscriptions = dict()
+        # publish_state
+        self.subscribe(self.publish_state, None, 'publish_state')
+        await self.setup()
+
+    async def _teardown(self):
+        await self.teardown()
+        for sender, topic in self._subscriptions.keys():
+            self.broker.unsubscribe(sender, topic)
+        self._subscriptions = dict()
+
+    async def _loop(self):
+        while True:
+            try:
+                await self.loop()
+                self.log.warning('loop should run forever')
+            except Exception as error:
+                self.log.exception('loop error', error)
+            await asyncio.sleep(10)
+
+    async def _message_received(self, sender=None, topic=None, payload=None):
+        self.log.debug('message_received from {} - {} {}'.format(sender, topic, payload))
+        await self.message_received(sender=sender, topic=topic, payload=payload)
+
+    async def publish_state(self, **kwargs):
+        self.log.debug('publish_state')
+        for topic, payload in self._state.items():
+            self.broker.publish(topic=topic, payload=payload)
+
+    def set_state(self, topic, payload):
+        self.log.debug('{} {}'.format(topic, payload))
+        self._state[topic] = payload
+        self.broker.publish(topic=topic, payload=payload)
+
+    def subscribe(self, callback, sender=None, topic=None):
+        self.broker.subscribe(callback, sender=sender, topic=topic)
+        self._subscriptions[(sender, topic)] = callback
+
+    async def setup(self):
+        pass
+
+    async def teardown(self):
+        pass
+
+    async def loop(self):
+        while True:
+            await asyncio.sleep(10)
+
+    async def message_received(self, sender=None, topic=None, payload=None):
+        pass
--- src/brick/device/test.py
+++ src/brick/device/test.py
@@ -1,8 +1,9 @@
-from brick.device.base import Device
+from brick.device import Device, register_device
 import random
 import uasyncio as asyncio
 
 
+@register_device()
 class Random(Device):
     def __init__(self, delay=10, scale=100):
         self.delay = int(delay)
@@ -20,19 +21,3 @@ class Random(Device):
         if topic == 'delay':
             self.delay = int(payload)
             self.set_state('delay', self.delay)
-
-
-class StatePublish(Device):
-    def __init__(self, delay=10):
-        self.delay = int(delay)
-
-    async def setup(self):
-        self.subscribe(self.log_state)
-
-    async def log_state(self, sender=None, topic=None, payload=None):
-        self.log.debug('{}/{} {}'.format(sender, topic, payload))
-
-    async def loop(self):
-        while True:
-            self.broker.publish(topic='publish_state')
-            await asyncio.sleep(self.delay)
