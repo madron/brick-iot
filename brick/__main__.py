793cf81ccfaaa0514d66ffa979b624e17425ba7e
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2020-05-02 11:05:56 +0200

    change default config directory

--- brick/__main__.py
+++ brick/__main__.py
@@ -3,11 +3,13 @@ import os
 import time
 from brick.app import Application
 
+DEFAULT_CONFIG_DIR = os.path.join(os.getcwd(), 'config')
+
 
 def main():
     parser = argparse.ArgumentParser(description='Brick')
-    parser.add_argument('--config-dir', metavar='D', default=os.getcwd(), dest='config_dir',
-                        help='Configuration directory. Default: current directory')
+    parser.add_argument('--config-dir', metavar='D', default=DEFAULT_CONFIG_DIR, dest='config_dir',
+                        help='Configuration directory. Default: config directory')
     parser.add_argument('--persist-command', metavar='C', dest='persist_command',
                         help='Command to make config persistent. Example: "/sbin/lbu commit -d"')
     kwargs = vars(parser.parse_args())
