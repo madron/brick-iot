356bbd6a72b4fb9ae47564d00d36488c0623d1fb
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2020-05-03 23:18:25 +0200

    refactor ADS1115 device

--- brick/device/adc.py
+++ brick/device/adc.py
@@ -1,56 +1,22 @@
-import asyncio
-import gpiozero
-import smbus
-from brick.device import Device, register_device
+from brick.device import NumericSensor, register_device
 from brick.device.i2c import i2c_manager
 from brick.hardware.ti import ads1x15
 
 
 @register_device()
-class Ads1115(Device):
-    def __init__(
-        self,
-        channel=0,
-        delay=10,
-        conversion_factor=1,
-        decimals=6,
-        config_mode=False,
-        address=0x48,
-        i2c_bus=0,
-    ):
+class ADS1115(NumericSensor):
+    def __init__(self, channel=0, address=0x48, i2c_bus=0, **kwargs):
+        super().__init__(**kwargs)
         assert channel in (0, 1, 2 , 3)
         self.channel = channel
-        self.delay = int(delay)
-        self.conversion_factor = float(conversion_factor)
-        self.decimals = int(decimals)
-        self.config_mode = bool(config_mode)
         self.voltage_conversion_factor = 4.096 / 32768
         self.bus = i2c_manager.get_bus(i2c_bus)
         self.adc = ads1x15.ADS1115(self.bus, address=address)
 
     async def setup(self):
-        self.set_state('delay', self.delay)
-        self.set_state('decimals', self.decimals)
-        self.set_state('conversion_factor', self.conversion_factor)
+        await super().setup()
         await self.bus.open()
 
-    async def loop(self):
-        while True:
-            value = await self.adc.read_adc(self.channel)
-            value = round(value * self.voltage_conversion_factor * self.conversion_factor, self.decimals)
-            if self.decimals < 1:
-                value = int(value)
-            self.set_state('value', value)
-            await asyncio.sleep(self.delay)
-
-    async def message_received(self, sender=None, topic=None, payload=None):
-        if self.config_mode:
-            if topic == 'delay':
-                self.delay = int(payload)
-                self.set_state('delay', self.delay)
-            if topic == 'conversion_factor':
-                self.conversion_factor = float(payload)
-                self.set_state('conversion_factor', self.conversion_factor)
-            if topic == 'decimals':
-                self.decimals = int(payload)
-                self.set_state('decimals', self.decimals)
+    async def get_value(self):
+        value = await self.adc.read_adc(self.channel)
+        return value * self.voltage_conversion_factor
